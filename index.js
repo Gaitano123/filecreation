465806f0bd8e815c16f69b55db54553647311f54
Gaitano <gaitano.george@student.moringaschool.com>
2023-06-14 08:43:06 +0300

    updated

---
+++ index.js
@@ -0,0 +1,32 @@
+let Age = 19;
+const result = (Age>=18) ? "Can vote!" : "Enjoy your childhood!";
+console.log(result);
+
+const score= 55;
+Switch (grade) {
+  case score >=80:
+  grade ='A';
+  break;
+  
+  case score >=70:
+  grade ='B';
+  break;
+
+  case score >=60:
+  grade ='C';
+  break;
+
+  case score >=50:
+  grade ='D';
+  break;
+
+  case score >=40:
+  grade ='E';
+  break;
+
+  default:
+  grade ='fail';
+  break;
+}
+
+console.log(grade);
